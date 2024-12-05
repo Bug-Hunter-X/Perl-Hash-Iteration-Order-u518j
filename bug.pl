eec63b94a863102cf71e88f16a3aa8c8fc3dbe89
BugHunter-X <askzenai@gmail.com>
2024-12-05 22:28:05 +0100

    Add bug.pl file

---
+++ bug.pl
@@ -0,0 +1,11 @@
+my %hash = (
+    'a' => 1,
+    'b' => 2,
+    'c' => 3
+);
+
+foreach my $key (keys %hash) {
+    print "Key: $key, Value: $hash{$key}\n";
+}
+
+#This will not print all the values correctly if the keys are not in alphabetical order.
